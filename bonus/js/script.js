4364dfc31b9538286f8db6a2eb3acaac6680f4d9
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-11-03 17:07:17 +0100

    Bonus
    Il pallino blu si sposta a destra e a sinistra coerentemente con i click del mouse o tastiera

---
+++ bonus/js/script.js
@@ -0,0 +1,68 @@
+// Descrizione:
+// Bonus:
+// i pallini si evidenziano in accordo alla img corrispondente
+// Clicchiamo sui pallini e mostriamo l’immagine corrispondente
+// altro esempio: Generiamo i pallini con JS (ma quello che volete liberamente)
+
+
+$(document).ready(function () {
+
+  $(".next").click(nextImg);
+  function nextImg() {
+    console.log("hai cliccato su next!!");
+
+    var imgAttiva = $("img.active");
+    imgAttiva.removeClass("active");
+
+    if (imgAttiva.hasClass("last")){
+      $("img.first").addClass("active");
+    } else {
+      imgAttiva.next("img").addClass("active");
+    }
+    //sposto la classe active sulle icone nel nav
+    var pallinoAttivo = $("i.active");
+    pallinoAttivo.removeClass("active");
+
+    if (pallinoAttivo.hasClass("last")){
+      $("i.first").addClass("active");
+    } else {
+      pallinoAttivo.next("i").addClass("active");
+    }
+  }
+
+  $(".prev").click(prevImg);
+  function prevImg() {
+    console.log("hai cliccato su prev!!");
+    var imgAttiva = $("img.active");
+    imgAttiva.removeClass("active");
+    if (imgAttiva.hasClass("first")){
+      $("img.last").addClass("active");
+    } else {
+      imgAttiva.prev("img").addClass("active");
+    }
+
+    //sposto la classe active sulle icone nel nav
+    var pallinoAttivo = $("i.active");
+    pallinoAttivo.removeClass("active");
+
+    if (pallinoAttivo.hasClass("first")){
+      $("i.last").addClass("active");
+    } else {
+      pallinoAttivo.prev("i").addClass("active");
+    }
+
+  }
+
+  $(document).keydown(function (schiacciaFrecce){
+    if (schiacciaFrecce.keyCode == 37) {
+      prevImg();
+      console.log('hai premuto la freccia sinistra');
+    }
+    if (schiacciaFrecce.keyCode == 39) {
+      nextImg();
+      console.log('hai premuto la freccia destra');
+    }
+  });
+
+
+});
